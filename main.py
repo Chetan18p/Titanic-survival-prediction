50e04c9f50b4e614f4cdc6e3a089f07366630c1b
Chetan18p <chetanphad2005@gmail.com>
2025-08-08 12:39:41 +0530

    Commit to stop for while

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('MarvellousTitanicDataset.csv')
+
+copy_df = df
+print()
+print()
+
+# printing the first 5 rows 
+print(df.head())
+print()
+print()
+
+# printing total number of null values
+print("Total number of null values in each column: ")
+print(df.isnull().sum())
+print()
+print()
+
+# Our dataset is clean and ready for analysis.
+
+#Visualizing survived and not survived passenger
+
+sns.countplot(x='Survived', data=df)
+plt.title('Survived vs Not Survived')
+plt.show()
